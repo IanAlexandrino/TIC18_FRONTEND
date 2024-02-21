339d370646423559ca287fadabd6c52258a0a095
Ian <ianrodrigues2001@hotmail.com>
2024-02-21 15:59:50 -0300

    Instalando firebase e adicionando componentelistar e form-adicionar

--- semana_12/aula_21_02_24/projeto_firebase/src/app/app.module.ts
+++ semana_12/aula_21_02_24/projeto_firebase/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FormAdicionarComponent } from './form-adicionar/form-adicionar.component';
+import { ComponentelistarComponent } from './componentelistar/componentelistar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FormAdicionarComponent,
+    ComponentelistarComponent
   ],
   imports: [
     BrowserModule,
